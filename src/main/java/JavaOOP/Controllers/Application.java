988005294449ffec1328bdb428a859bc282d085e
Vladislav Stecenko <st.mcv1ad@gmail.com>
2017-11-20 13:22:56 +0200

    Task 2 v2.0

--- src/main/java/JavaOOP/Controllers/Application.java
+++ src/main/java/JavaOOP/Controllers/Application.java
@@ -1,13 +1,14 @@
 package JavaOOP.Controllers;
 
 import JavaOOP.IO.Input;
-import JavaOOP.Model.Book;
+import JavaOOP.IO.Output;
 
 import java.io.IOException;
 
 public class Application {
     public void run() throws IOException {
         Controller controller = new Controller();
+        Output printer = new Output();
         int k;
         do {
             System.out.println("" +
@@ -18,15 +19,19 @@ public class Application {
             k = Input.readInteger();
             switch (k) {
                 case 1: {
+                    printer.printBooks(controller.getBooksByAuthor(Input.readString()));
                     break;
                 }
                 case 2: {
+                    printer.printBooks(controller.getBooksByPublishingHouse(Input.readString()));
                     break;
                 }
                 case 3: {
+                    printer.printBooks(controller.getBooksByYear(Input.readInteger()));
                     break;
                 }
                 case 4: {
+                    printer.printBooks(controller.sortBooksByPublishingHouse());
                     break;
                 }
                 case 5: {
