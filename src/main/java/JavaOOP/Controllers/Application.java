9f605196b2f9f1c67202e2f5197fb8b46c1342c3
Vladislav Stecenko <st.mcv1ad@gmail.com>
2017-11-20 13:19:45 +0200

    Task 2 fix

--- src/main/java/JavaOOP/Controllers/Application.java
+++ src/main/java/JavaOOP/Controllers/Application.java
@@ -1,32 +1,38 @@
 package JavaOOP.Controllers;
 
 import JavaOOP.IO.Input;
-import JavaOOP.Model.Book;
+import JavaOOP.IO.Output;
 
 import java.io.IOException;
 
 public class Application {
     public void run() throws IOException {
         Controller controller = new Controller();
+        Output printer = new Output();
         int k;
         do {
             System.out.println("" +
                     "(1)Get books by author.\n" +
                     "(2)Get books by publishing house.\n" +
+                    "(3)Get books that was published after specified year.\n" +
                     "(4)Sort books by publishing house.\n" +
                     "(5)Exit.");
             k = Input.readInteger();
             switch (k) {
                 case 1: {
+                    printer.printBooks(controller.getBooksByAuthor(Input.readString()));
                     break;
                 }
                 case 2: {
+                    printer.printBooks(controller.getBooksByPublishingHouse(Input.readString()));
                     break;
                 }
                 case 3: {
+                    printer.printBooks(controller.getBooksByYear(Input.readInteger()));
                     break;
                 }
                 case 4: {
+                    printer.printBooks(controller.sortBooksByPublishingHouse());
                     break;
                 }
                 case 5: {
@@ -37,5 +43,6 @@ public class Application {
             }
         } while (true);
     }
+
 
 }
